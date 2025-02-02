77806317c048315d1edf2f243175e81507811867
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-02-02 02:24:13 +0200

    Form-Validation-Creation

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ document.addEventListener("DOMContentLoaded", function (){
 document.getElementById("registration-form")
 
 const form = "registration-form"
+
+document.addEventListener("DOMContentLoaded", function(){
+    const feedbackDiv = document.ElementById("form-feedback");
+ })
